4bdaf5674a4040dfe46c7a8e0e06a2fd59c39efc
Stephan Sundermann <stephansundermann@gmail.com>
2025-07-20 23:16:08 +0200

    [webOS] Split video and audio player into header / cpp

--- xbmc/cores/VideoPlayer/VideoPlayerAudioWebOS.h
+++ xbmc/cores/VideoPlayer/VideoPlayerAudioWebOS.h
@@ -18,33 +18,24 @@
 class CVideoPlayerAudioWebOS final : public IDVDStreamPlayerAudio
 {
 public:
-  CVideoPlayerAudioWebOS(CMediaPipelineWebOS& mediaPipeline, CProcessInfo& processInfo)
-    : IDVDStreamPlayerAudio(processInfo), m_mediaPipeline(mediaPipeline)
-  {
-  }
-  void FlushMessages() override { m_mediaPipeline.FlushAudioMessages(); };
-  bool OpenStream(CDVDStreamInfo hints) override { return m_mediaPipeline.OpenAudioStream(hints); }
-  void CloseStream(const bool waitForBuffers) override
-  {
-    m_mediaPipeline.CloseAudioStream(waitForBuffers);
-  }
-  void SetSpeed(const int speed) override { m_mediaPipeline.SetSpeed(speed); }
-  void Flush(const bool sync) override { m_mediaPipeline.Flush(sync); }
-  [[nodiscard]] bool AcceptsData() const override { return m_mediaPipeline.AcceptsAudioData(); }
-  [[nodiscard]] bool HasData() const override { return m_mediaPipeline.HasAudioData(); }
-  [[nodiscard]] int GetLevel() const override { return m_mediaPipeline.GetAudioLevel(); }
-  [[nodiscard]] bool IsInited() const override { return m_mediaPipeline.IsAudioInited(); }
-  void SendMessage(const std::shared_ptr<CDVDMsg> msg, const int priority) override
-  {
-    m_mediaPipeline.SendAudioMessage(msg, priority);
-  }
-  void SetDynamicRangeCompression(long drc) override {}
-  std::string GetPlayerInfo() override { return m_mediaPipeline.GetAudioInfo(); }
-  int GetAudioChannels() override { return m_mediaPipeline.GetAudioChannels(); }
-  double GetCurrentPts() override { return m_mediaPipeline.GetCurrentPts(); }
-  [[nodiscard]] bool IsStalled() const override { return m_mediaPipeline.IsStalled(); }
-  [[nodiscard]] bool IsPassthrough() const override { return true; }
-  [[nodiscard]] float GetDynamicRangeAmplification() const override { return 0.0f; }
+  CVideoPlayerAudioWebOS(CMediaPipelineWebOS& mediaPipeline, CProcessInfo& processInfo);
+  void FlushMessages() override;
+  bool OpenStream(CDVDStreamInfo hints) override;
+  void CloseStream(const bool waitForBuffers) override;
+  void SetSpeed(const int speed) override;
+  void Flush(const bool sync) override;
+  [[nodiscard]] bool AcceptsData() const override;
+  [[nodiscard]] bool HasData() const override;
+  [[nodiscard]] int GetLevel() const override;
+  [[nodiscard]] bool IsInited() const override;
+  void SendMessage(const std::shared_ptr<CDVDMsg> msg, const int priority) override;
+  void SetDynamicRangeCompression(long drc) override;
+  std::string GetPlayerInfo() override;
+  int GetAudioChannels() override;
+  double GetCurrentPts() override;
+  [[nodiscard]] bool IsStalled() const override;
+  [[nodiscard]] bool IsPassthrough() const override;
+  [[nodiscard]] float GetDynamicRangeAmplification() const override;
 
 private:
   CMediaPipelineWebOS& m_mediaPipeline;
--- xbmc/cores/VideoPlayer/VideoPlayerVideoWebOS.h
+++ xbmc/cores/VideoPlayer/VideoPlayerVideoWebOS.h
@@ -20,37 +20,25 @@
 class CVideoPlayerVideoWebOS final : public IDVDStreamPlayerVideo
 {
 public:
-  CVideoPlayerVideoWebOS(CMediaPipelineWebOS& mediaPipeline, CProcessInfo& processInfo)
-    : IDVDStreamPlayerVideo(processInfo), m_mediaPipeline(mediaPipeline)
-  {
-  }
-  void FlushMessages() override { m_mediaPipeline.FlushVideoMessages(); }
-  bool OpenStream(const CDVDStreamInfo hint) override
-  {
-    return m_mediaPipeline.OpenVideoStream(hint);
-  }
-  void CloseStream(const bool waitForBuffers) override
-  {
-    m_mediaPipeline.CloseVideoStream(waitForBuffers);
-  }
-  void Flush(const bool sync) override { m_mediaPipeline.Flush(sync); }
-  [[nodiscard]] bool AcceptsData() const override { return m_mediaPipeline.AcceptsVideoData(); }
-  [[nodiscard]] bool HasData() const override { return m_mediaPipeline.HasVideoData(); }
-  [[nodiscard]] bool IsInited() const override { return m_mediaPipeline.IsVideoInited(); }
-  void SendMessage(const std::shared_ptr<CDVDMsg> msg, const int priority) override
-  {
-    m_mediaPipeline.SendVideoMessage(msg, priority);
-  }
-  void EnableSubtitle(const bool enable) override { m_mediaPipeline.EnableSubtitle(enable); }
-  bool IsSubtitleEnabled() override { return m_mediaPipeline.IsSubtitleEnabled(); }
-  double GetSubtitleDelay() override { return m_mediaPipeline.GetSubtitleDelay(); }
-  void SetSubtitleDelay(const double delay) override { m_mediaPipeline.SetSubtitleDelay(delay); }
-  [[nodiscard]] bool IsStalled() const override { return m_mediaPipeline.IsStalled(); }
-  double GetCurrentPts() override { return m_mediaPipeline.GetCurrentPts(); }
-  double GetOutputDelay() override { return 0.0; }
-  std::string GetPlayerInfo() override { return m_mediaPipeline.GetVideoInfo(); }
-  int GetVideoBitrate() override { return m_mediaPipeline.GetVideoBitrate(); }
-  void SetSpeed(const int speed) override { m_mediaPipeline.SetSpeed(speed); }
+  CVideoPlayerVideoWebOS(CMediaPipelineWebOS& mediaPipeline, CProcessInfo& processInfo);
+  void FlushMessages() override;
+  bool OpenStream(const CDVDStreamInfo hint) override;
+  void CloseStream(const bool waitForBuffers) override;
+  void Flush(const bool sync) override;
+  [[nodiscard]] bool AcceptsData() const override;
+  [[nodiscard]] bool HasData() const override;
+  [[nodiscard]] bool IsInited() const override;
+  void SendMessage(const std::shared_ptr<CDVDMsg> msg, const int priority) override;
+  void EnableSubtitle(const bool enable) override;
+  bool IsSubtitleEnabled() override;
+  double GetSubtitleDelay() override;
+  void SetSubtitleDelay(const double delay) override;
+  [[nodiscard]] bool IsStalled() const override;
+  double GetCurrentPts() override;
+  double GetOutputDelay() override;
+  std::string GetPlayerInfo() override;
+  int GetVideoBitrate() override;
+  void SetSpeed(const int speed) override;
 
 private:
   CMediaPipelineWebOS& m_mediaPipeline;
